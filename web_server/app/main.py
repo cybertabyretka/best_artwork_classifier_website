c51a32767833964729af6324dff26db258955db7
Vlad Kalashnikov <kalashnikovv537@gmail.com>
2025-07-11 01:29:57 +0500

    fixed path to csv file

--- web_server/app/main.py
+++ web_server/app/main.py
@@ -7,7 +7,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from .utils import load_artists_mapping
 
-ARTISTS_MAPPING = load_artists_mapping('../artists.csv')
+ARTISTS_MAPPING = load_artists_mapping('app/artists.csv')
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
